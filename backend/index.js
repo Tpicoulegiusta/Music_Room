9c73e7ab93d68f79b77892708b5a76f424adb83a
Tpicoulegiusta <tpicoule123@gmail.com>
2026-02-26 14:58:17 +0100

    serv begining

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+
+
+
+const express = require('express'); //import de express comme un iclude//
+const app = express(); //mon serveur(app) est le serveur express (vide)//
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Hello from Music Room server');
+}); //reponse du serveur basique lors d'une connexion a la page 'HOME'('/')//
+
+app.listen(port, () => {
+	console.log("Server running on port ${port}")
+}); //le serveur ecoute le port 3000//
