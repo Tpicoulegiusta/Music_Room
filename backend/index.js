cd2d3f497c7457b03ecba772c7e37348493be097
Tpicoulegiusta <tpicoule123@gmail.com>
2026-02-27 15:32:20 +0100

    avancement sur le serv

--- backend/index.js
+++ backend/index.js
@@ -6,9 +6,25 @@ const app = express(); //mon serveur(app) est le serveur express (vide)//
 const port = 3000;
 
 app.get('/', (req, res) => {
-	res.send('Hello from Music Room server');
+	res.send('Hello from Music Room server\n');
 }); //reponse du serveur basique lors d'une connexion a la page 'HOME'('/')//
 
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
 }); //le serveur ecoute le port 3000//
+
+app.get('/rooms', (req, res) => {
+	res.json([
+		{id: 1, name: 'Jazz Club', users: 3},
+		{id: 2, name: 'Rock Arena', users: 12}
+	]);
+}); // reponse du serveur lorsque le client arrive dans /rooms//
+
+app.get('/rooms/:id', (req, res) => {
+	const roomId = req.params.id; //express extrait automatiquement l'id pour le mettre dans param//
+	res.json({
+		id: roomId,
+		name: `Room ${roomId}`,
+		users: 5
+	});
+}); //reponse si url dynamique (id de la room different)//
