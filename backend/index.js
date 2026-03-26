e1ed79ef6312c25e893298ca69c79cd630ddbd51
Tpicoulegiusta <tpicoule123@gmail.com>
2026-03-26 14:45:26 +0100

    added routes, cmd ...

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,15 @@ const express = require('express'); //import de express comme un iclude//
 const app = express(); //mon serveur(app) est le serveur express (vide)//
 const port = 3000;
 
+let rooms = [
+	{ id: 1, name: 'Jazz Club', users: 3 },
+	{ id: 2, name: 'Gay Rock Arena', users: 12 }
+];
+
+
+app.use(express.json());
+
+
 app.get('/', (req, res) => {
 	res.send('Hello from Music Room server\n');
 }); //reponse du serveur basique lors d'une connexion a la page 'HOME'('/')//
@@ -14,17 +23,62 @@ app.listen(port, () => {
 }); //le serveur ecoute le port 3000//
 
 app.get('/rooms', (req, res) => {
-	res.json([
-		{id: 1, name: 'Jazz Club', users: 3},
-		{id: 2, name: 'Rock Arena', users: 12}
-	]);
+	res.json(rooms);
 }); // reponse du serveur lorsque le client arrive dans /rooms//
 
 app.get('/rooms/:id', (req, res) => {
-	const roomId = req.params.id; //express extrait automatiquement l'id pour le mettre dans param//
-	res.json({
-		id: roomId,
-		name: `Room ${roomId}`,
-		users: 5
-	});
+	const roomId = Number(req.params.id); //express extrait 
+	//automatiquement l'id pour le mettre dans param//
+	const room = rooms.find(r => r.id == roomId);
+
+	if (!room){
+		return res.status(404).json({ error: 'Room not found' });
+	}
+
+	res.json(room);
 }); //reponse si url dynamique (id de la room different)//
+
+app.post('/rooms', (req, res) => {
+	const newRoom = {
+		id: Date.now(),
+		name: req.body.name || 'New Room',
+		users: 0
+	};
+
+	rooms.push(newRoom);
+	
+	res.status(201).json(newRoom);
+});//Une route POST creer/envoie des donnees contrairement a get qui se contente de lire//
+
+app.put('/rooms/:id', (req, res) => {
+	const roomId = Number(req.params.id);
+	const room = rooms.find(r => r.id === roomId);
+
+	if (!room) {
+		return res.status(404).json({ error: 'Room not found' });
+	}
+
+	if (req.body.name !== undefined) {
+		room.name = req.body.name;
+	}
+
+	if (req.body.users !== undefined) {
+		room.users = req.body.users;
+	}
+
+	res.json(room);
+});// Une route pour changer les rooms existantes//
+
+app.delete('/rooms/:id', (req, res) => {
+	const roomId = Number(req.params.id);
+
+	const index = rooms.findIndex(r => r.id === roomId);
+
+	if(index === -1) {
+		return res.status(404).json({ error: 'Room not found' });
+	}
+	//delete un element a la pos index//
+	const deleted = rooms.splice(index, 1)[0];
+
+	res.json({ message: 'Room deleted', room: deleted});
+});
